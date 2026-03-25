12cda29b21f1bcdcbd528d4b582906d474dd9616
IaK3lwin <krochaalmeida7@gmail.com>
2026-03-25 16:14:41 -0300

    finish commit

---
+++ src/main/java/dev/magalu_alt/message_notify/MagaluAltApplication.java
@@ -0,0 +1,13 @@
+package dev.magalu_alt.message_notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagaluAltApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MagaluAltApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/message_broker/magalu_alt/MagaluAltApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.message_broker.magalu_alt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluAltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
