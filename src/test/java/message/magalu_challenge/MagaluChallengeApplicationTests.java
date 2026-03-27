b9bb014831a276a7ba9a5d652822c25954021683
IaK3lwin <krochaalmeida7@gmail.com>
2026-03-27 12:11:48 -0300

    finish init real

---
+++ src/test/java/message/magalu_challenge/MagaluChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package message.magalu_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaluChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
