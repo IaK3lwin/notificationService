2578abd54422e5b5ac51cb1011f2a638469aae31
IaK3lwin <krochaalmeida7@gmail.com>
2026-03-27 12:03:39 -0300

    reset all

--- src/main/java/dev/magalu_alt/message_notify/domain/usecases/channel/ChannelAdapter.java
+++
@@ -1,7 +0,0 @@
-package dev.magalu_alt.message_notify.domain.usecases.channel;
-
-public interface ChannelAdapter {
-  public Long getId();
-
-  public String getDescription();
-}
--- src/main/java/dev/magalu_alt/message_notify/domain/usecases/status/NotificationStatusAdapter.java
+++
@@ -1,7 +0,0 @@
-package dev.magalu_alt.message_notify.domain.usecases.status;
-
-public interface NotificationStatusAdapter {
-  public Long getId();
-
-  public String getValue();
-}
